2fba85899596ebd284085c4a50de4d2da7a1253d
yoonjk <nexweb1@gmail.com>
2022-11-06 20:34:07 +0900

    Add flask example

--- lab04/hello.py
+++ lab04/hello.py
@@ -1,9 +1,11 @@
+from flask_wtf import FlaskForm
+from wtforms import StringField, SubmitField
+from wtforms.validators import DataRequired
 from flask import Flask, render_template
 from flask_bootstrap import Bootstrap
 
 app = Flask(__name__)
 app.config['TEMPLATES_AUTO_RELOAD'] = True
-app.config['SECRET_KEY'] = 'hard to guess string'
 bootstrap = Bootstrap(app)
 
 @app.route('/')
@@ -13,3 +15,7 @@ def index():
 @app.route('/user/<name>')
 def user(name):
     return render_template('user.html', name=name)
+
+class NameForm(FlaskForm):
+    name = StringField('What is your name?', validators=[DataRequired()])
+    submit = SubmitField('Submit')
